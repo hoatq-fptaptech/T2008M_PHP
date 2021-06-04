098a457f2d9cd11b71d2535033599e2d54c2f900
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-06-04 09:08:04 +0700

    up code database file

--- capnhat.php
+++ capnhat.php
@@ -3,21 +3,11 @@
     $age = $_POST["age"];
     $address = $_POST["address"];
     $id = $_POST["id"];
-    $servername = "localhost";
-    $username = "root";
-    $password = ""; // neu dung mamp password: root
-    $db = "t2008m_php";
-    // create connection
-    $conn = new mysqli($servername,$username,$password,$db);
-    // kiem tra ket noi
-    if($conn->connect_error){
-        die("Connect error...");// die lam dung luong chuong trinh tai day
-    }
+
     $sql_txt = "update sinhviens set name='$name',age = $age, address='$address' where id= $id";
-    if($conn->query($sql_txt) === true){
-        header("location: list.php"); // readirect ve trang danh sach
+
+    if(insertOrUpdateDB($sql_txt)){
+        header("Location: list.php");
     }else{
-        echo "ERRORS:".$conn->error;
+        echo "Error";
     }
-    // array key=>value
-//object obj->method() object->attribute
--- list.php
+++ list.php
@@ -1,3 +1,4 @@
+<?php include_once "database.php" ;?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -10,27 +11,9 @@
 <body>
     <h1>Danh sách sinh viên</h1>
     <?php
-        // lay ds sv tu database
-        $servername = "localhost";
-        $username = "root";
-        $password = ""; // neu dung mamp password: root
-        $db = "t2008m_php";
-        // create connection
-        $conn = new mysqli($servername,$username,$password,$db);
-        // kiem tra ket noi
-        if($conn->connect_error){
-            die("Connect error...");// die lam dung luong chuong trinh tai day
-        }
-      //  echo "Connect successfully";
         // lay data tu bang sinhviens
         $sql_txt = "select * from sinhviens";
-        $rs = $conn->query($sql_txt);
-        $dssinhvien = [];
-        if($rs->num_rows>0){
-            while($row = $rs->fetch_assoc()){
-                $dssinhvien[] = $row;
-            }
-        }
+        $dssinhvien = queryDB($sql_txt);
     ?>
     <a href="themsinhvien.php">Thêm sinh viên</a>
     <ul>
